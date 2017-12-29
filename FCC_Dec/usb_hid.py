9af004c72b663bd4607acd23536aec4d999c1c2e
Christian Muller <Christian.Muller@magtek.com>
2017-12-29 09:49:26 -0800

    HID: Able to write and read ICC_STATUS

--- FCC_Dec/usb_hid.py
+++ FCC_Dec/usb_hid.py
@@ -1,15 +1,23 @@
+#Use this one
+
+
 import usb.core
 import usb.util
 import pywinusb.hid as hid
+import time
 
 # find our device
 # dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
 
 def readData(data):
-    print(data)
-    return None
-
-
+	#print "RX->: "
+	#print [hex (l) for l in data]
+	if data[12] != 0:
+		print "USB FAILED!!!"
+		return None
+	print "USB PASSED!!!"
+	
+	return None
 
 
 cvendor_id=0x0801
@@ -20,36 +28,17 @@ dev = hid.HidDeviceFilter(vendor_id = 0x0801, product_id = 0x001B).get_devices()
 if dev is None:
     raise ValueError('Device not found')
 
-#dev.set_configuration()
-#dev.set_raw_data_handler(readData)
+#hid.set_configuration()
+dev.open()
+dev.set_raw_data_handler(readData)
 
-report = dev.find_output_reports()[0]
+report = dev.find_feature_reports()[0]
 
-buffer= [0xFF]*33 # 33 = report size + 1 byte (report id)
-buffer[0]=0x0 # report id
-buffer[1]=0xFE
-buffer[2]=0x00
-buffer[3]=0xFF
-out_report.set_raw_data(buffer)
-out_report.send()
+#ICC STATUS
+buffer = bytearray.fromhex(u' 05 C0 01 01 C1 01 06 C2 01 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00')
+buffer[0] = 05
+report.set_raw_data(buffer)
+report.send()
+
+time.sleep(1)
 dev.close()
-
-
-
-
-cfg=dev[0]
-intf=cfg[(0,0)]
-ep=intf[0]
-
-
-
-
-#dev.write(ep.bEndpointAddress, [0x00, 0x00,0x04,0x04,0xFF,0xFF,0xFF,0x00, 0x00], 1000)
-#dev.ctrl_transfer(bmRequestType, bRequest, wValue=0, wIndex=0, data_or_wLength=None, timeout=None)
-
-print("print ep")
-print(ep)
-print("print cfg")
-print(cfg)
-print("print intf")
-print(intf)
