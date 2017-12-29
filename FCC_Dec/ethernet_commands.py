785788352b901f855d9397fb71d96068227a7ffc
Christian Muller <Christian.Muller@magtek.com>
2017-12-29 11:34:40 -0800

    Ethernet: Read and Write

--- FCC_Dec/ethernet_commands.py
+++ FCC_Dec/ethernet_commands.py
@@ -2,69 +2,85 @@ import socket
 import array
 import time
 import binascii
+import struct
 
-#print 'Please enter IP address: '
-#dynaproIP = raw_input('IP address [10.57.10.133]: ')
-#if dynaproIP == "":
-dynaproIP = "10.57.22.123"
 
-#dynaproPort = raw_input('Port [26]: ')
-#if dynaproPort == "":
-dynaproPort = str(5000)
+#convert string to hex
+def toHex(s):
+	lst = []
+	for ch in s:
+		hv = hex(ord(ch)).replace('0x', ' ')
+		if len(hv) == 1:
+			hv = '0'+hv
+		lst.append(hv)
+	return reduce(lambda x,y:x+y, lst)
 
-print "Attempting to connect to DynaPro at " + dynaproIP + " on " + dynaproPort
+#convert hex repr to string
+def toStr(s):
+	return s and chr(int(s[:2], base=16)) + toStr(s[2:]) or ''
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((dynaproIP, int(dynaproPort)))
 
-sendCommand_tamper = bytearray.fromhex(u'C0 01 01 C1 01 01 C2 01 3F')
-sendCommand_icc_read = bytearray.fromhex(u'C0 01 01 C1 01 06 C2 01 01')
+def eth_test(app, cmd):
+	status = -1
+	#print 'Please enter IP address: '
+	#dynaproIP = raw_input('IP address [10.57.10.133]: ')
+	#if dynaproIP == "":
+	dynaproIP = "10.57.22.123"
 
-counter = 0
-while (1):
-	counter += 1
-	if counter > 500:
-		break
+	#dynaproPort = raw_input('Port [26]: ')
+	#if dynaproPort == "":
+	dynaproPort = str(5000)
 
-	print "-> " + binascii.hexlify(sendCommand_tamper)
-	client_socket.send(sendCommand_tamper)
+	print "Attempting to connect to DynaPro at " + dynaproIP + " on " + dynaproPort
+
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client_socket.connect((dynaproIP, int(dynaproPort)))
+
+
+	buffer = bytearray.fromhex(u'C0 01 01 C1 01 01 C2 01 3F')
+	#sendCommand_icc_read = bytearray.fromhex(u'C0 01 01 C1 01 06 C2 01 01')
+
+	
+	buffer[5] = app
+	buffer[8] = cmd
+	
+#	counter = 0
+#	while (1):
+#		counter += 1
+#		if counter > 500:
+#			break
+
+	print "-> " + binascii.hexlify(buffer)
+	client_socket.send(buffer)
 	recvData = client_socket.recv(10000)
+	
+	print len(recvData)
+
+	#data_list = map(ord, recvData)
+	#print data_list
+	
+	data_list = struct.unpack(str(len(recvData))+'B', recvData)
+	print data_list
+
+	if data_list[11] != 0:
+		print "ETHER FAILED!!!"
+		status = 1
+	print "ETH PASSED!!!"
+	status = 0
+
+	
 	print "<- " + binascii.hexlify(recvData)
 
-	print "-> " + binascii.hexlify(sendCommand_icc_read)
-	client_socket.send(sendCommand_icc_read)
-	recvData = client_socket.recv(10000)
-	print "<- " + binascii.hexlify(recvData)
-	print counter
-	
-	time.sleep(5)
+#		print "-> " + binascii.hexlify(sendCommand_icc_read)
+#		client_socket.send(sendCommand_icc_read)
+#		recvData = client_socket.recv(10000)
+#		print "<- " + binascii.hexlify(recvData)
+#		print counter
+		
+	time.sleep(1)
+	client_socket.close()
+	return status
 
-client_socket.close()
-
-
-
-
-
-
-#while(1):
-#	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#	client_socket.connect((dynaproIP, int(dynaproPort)))
-#	print "-> " + binascii.hexlify(sendCommand)
-#	client_socket.send(sendCommand)
-#	time.sleep(1)
-#	recvData = client_socket.recv(10000)
-#	print "<- " + binascii.hexlify(recvData)
-	
-#	client_socket.send(sendCommand)
-#	recvData = client_socket.recv(10000)
-#	print "<- " + binascii.hexlify(recvData)
-
-	
-	#client_socket.close()
-#time.sleep(1)
-#recvData = client_socket.recv(10000)
-#print "<- " + binascii.hexlify(recvData)
-
-#print
-
-#client_socket.close()
+if __name__=='__main__':
+	print "ret is: " + str(eth_test(6,1))
+	print "ret is: " + str(eth_test(1,0x3F))
